1c3a82a8984e95d61abf5ad1ac570bdaf9871eec
Julieta Azzini <juliazzini1@gmail.com>
2021-04-23 02:16:15 -0300

    working on api key and fetch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  async function fetchData() {
+    const accessToken = process.env.REACT_APP_API_KEY;
+    console.log(accessToken);
+    const data = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?id=3433955&appid=${accessToken}`
+    );
+    const dataJson = await data.json();
+    console.log(dataJson);
+  }
+
+  return <div>Hola mundo</div>;
 }
 
 export default App;
